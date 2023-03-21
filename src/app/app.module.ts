cc14779768c0b91fec94a93215d6b8935efed160
Ernesto Herrera <ernerdo@gmail.com>
2023-03-20 20:53:32 -0600

    add table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { DialogModule } from '@angular/cdk/dialog';
 import { ScrollingModule } from '@angular/cdk/scrolling';
+import { CdkTableModule } from '@angular/cdk/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { BoardComponent } from './pages/board/board.component';
 import { TodoDialogComponent } from './components/todo-dialog/todo-dialog.component';
 import { ScrollComponent } from './pages/scroll/scroll.component';
+import { TableComponent } from './pages/table/table.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +30,7 @@ import { ScrollComponent } from './pages/scroll/scroll.component';
     BoardComponent,
     TodoDialogComponent,
     ScrollComponent,
+    TableComponent,
   ],
   imports: [
     BrowserModule,
@@ -39,6 +42,7 @@ import { ScrollComponent } from './pages/scroll/scroll.component';
     DialogModule,
     HttpClientModule,
     ScrollingModule,
+    CdkTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
