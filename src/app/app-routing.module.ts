571e6d9e8fdf59256c7d5951651097852258ef2f
Ernesto Herrera <ernerdo@gmail.com>
2023-03-24 20:21:50 -0600

    add protect route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from '@guards/auth.guard';
+
 const routes: Routes = [
   {
     path: '',
@@ -9,6 +11,7 @@ const routes: Routes = [
   },
   {
     path: 'app',
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import('./modules/layout/layout.module').then((m) => m.LayoutModule),
   },
